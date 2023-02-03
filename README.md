529f83a975fa5d170cae28e1706c35b6edf8f708
GamerNoTitle <GamerNoTitle@outlook.com>
2023-02-03 08:28:08 +0800

    Repost

---
+++ README.md
@@ -0,0 +1,42 @@
+# MHYY-AutoCheckin
+
+![MHYY-AutoCheckin](https://socialify.git.ci/ElainaMoe/MHYY-AutoCheckin/image?forks=1&language=1&name=1&owner=1&stargazers=1&theme=Light)
+
+[米哈云游（云原神）](https://mhyy.mihoyo.com/)自动签到脚本
+
+**⚠️请不要进行宣传，谢谢！一旦发现宣传就删库跑路！使用过程中如果出现bug可能会使用您的日志进行错误追踪，详情请见[隐私政策](https://github.com/ElainaMoe/MHYY-AutoCheckin/blob/master/private-policy.md)**
+
+这是一个可以帮助你每天自动进行米忽悠云原神签到的脚本，自动获取每日的15分钟（600分钟后无法获取，这是米忽悠定下的规矩，无法突破）
+
+## 快速开始
+
+请参阅[文档](https://bili33.top/posts/MHYY-AutoCheckin-Manual/)进行配置，有问题请在issue中提出。
+
+备用文档地址：https://blog.ninym.top/posts/MHYY-AutoCheckin-Manual/
+
+**要是真的不会用就别不好意思开issue问啦，我在Sentry的后台看到了一堆不知道怎么形容的Bug**
+
+## 赞助
+点击下面的Badge其中一个就可以跳转到相应页面，感谢老板的支持！
+
+<a href="https://afdian.net/@GamerNoTitle"><img src="https://img.shields.io/badge/%E7%88%B1%E5%8F%91%E7%94%B5-GamerNoTitle-%238e8cd8?style=for-the-badge" alt="前往爱发电赞助" width=auto height=auto border="0" /></a> <a href="https://cdn.jsdelivr.net/gh/GamerNoTitle/Picture-repo@master/img/Donate/WeChatPay.png"><img src="https://img.shields.io/badge/%E5%BE%AE%E4%BF%A1%E6%94%AF%E4%BB%98-GamerNoTitle-%2304BE02?style=for-the-badge" alt="使用微信赞助" width=auto height=auto border="0" /></a> <a href="https://cdn.jsdelivr.net/gh/GamerNoTitle/Picture-repo@master/img/Donate/AliPay.jpg"><img src="https://img.shields.io/badge/%E6%94%AF%E4%BB%98%E5%AE%9D%E6%94%AF%E4%BB%98-GamerNoTitle-%231678FF?style=for-the-badge" alt="使用支付宝赞助" width=auto height=auto border="0" /></a>
+
+## 免责声明
+
+一旦你fork了本仓库则代表你同意以下内容：
+
+- 所有账号被封禁的情况由使用者自行承担
+- 一切由使用本脚本造成的后果由使用者自行承担
+
+## 友链
+
+[Node.js版的云原神15分钟获取器 带邮件发送日志功能 需自备服务器 HuanLinMaster/GenshinCloudPlayHelper](https://github.com/HuanLinMaster/GenshinCloudPlayHelper)
+
+## Contributions
+
+![Contribution](https://repobeats.axiom.co/api/embed/da9099b7978340d1d808225a3ae70f00c3b7b54d.svg "Repobeats analytics image")
+
+
+## Stargazers over time
+
+[![Stargazers over time](https://starchart.cc/ElainaMoe/MHYY-AutoCheckin.svg)](https://starchart.cc/ElainaMoe/MHYY-AutoCheckin)
---
+++ SCF/index.py
@@ -0,0 +1,118 @@
+import requests as r
+import json
+import re
+import urllib3
+import time
+import random
+
+with open('./config.json', 'rt') as f:   # SCF config loader
+    config = json.loads(f.read())
+    f.close()
+
+
+class RunError(Exception):
+    pass
+
+
+token = config['token']
+client_type = config['type']
+try:
+    ver_info = r.get('https://api-cloudgame-static.mihoyo.com/hk4e_cg_cn/gamer/api/getFunctionShieldNew?client_type=1').text
+    version = json.loads(ver_info)['data']['config']['cg.key_function_controller']['versions'][-1]
+    print(f'从官方API获取到云·原神最新版本号：{version}')
+except:
+    version = config['version']
+android = config['android']
+deviceid = config['deviceid']
+devicename = config['devicename']
+devicemodel = config['devicemodel']
+appid = config['appid']
+analytics = config['analytics']
+
+bbsid = re.findall(r'oi=[0-9]+', token)[0].replace('oi=', '')
+
+NotificationURL = 'https://api-cloudgame.mihoyo.com/hk4e_cg_cn/gamer/api/listNotifications?status=NotificationStatusUnread&type=NotificationTypePopup&is_sort=true'
+WalletURL = 'https://api-cloudgame.mihoyo.com/hk4e_cg_cn/wallet/wallet/get'
+AnnouncementURL = 'https://api-cloudgame.mihoyo.com/hk4e_cg_cn/gamer/api/getAnnouncementInfo'
+headers = {
+    'x-rpc-combo_token': token,
+    'x-rpc-client_type': str(client_type),
+    'x-rpc-app_version': str(version),
+    'x-rpc-sys_version': str(android),  # Previous version need to convert the type of this var
+    'x-rpc-channel': 'mihoyo',
+    'x-rpc-device_id': deviceid,
+    'x-rpc-device_name': devicename,
+    'x-rpc-device_model': devicemodel,
+    'x-rpc-app_id': str(appid),
+    'Referer': 'https://app.mihoyo.com',
+    'Host': 'api-cloudgame.mihoyo.com',
+    'Connection': 'Keep-Alive',
+    'Accept-Encoding': 'gzip',
+    'User-Agent': 'okhttp/4.9.0'
+}
+
+
+def handler(*args):
+    if config == '':
+        # Verify config
+        raise RunError(
+            f"请在Settings->Secrets->Actions页面中新建名为config的变量，并将你的配置填入后再运行！")
+    else:
+        if token == '' or android == 0 or deviceid == '' or devicemodel == '' or appid == 0:
+            raise RunError(f'请确认您的配置文件配置正确再运行本程序！')
+    if analytics:
+        try:
+            # Disable SSL warning of analytics server
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+            ana = r.get(
+                f'https://analytics.api.ninym.top/mhyy?type={client_type}&version={version}&android={android}&deviceid={deviceid}&devicename={devicename}&devicemodel={devicemodel}&appid={appid}&bbsid={bbsid}', verify=False)
+            if json.loads(ana.text)['msg'] == 'OK':
+                print('统计信息提交成功，感谢你的支持！')
+            elif json.loads(ana.text)['msg'] == 'Duplicated':
+                print('你的统计信息已经提交过啦！感谢你的支持！')
+            else:
+                print(f'[WARN] 统计信息提交错误：{ana.text}')
+        except Exception as e:
+            print(f'提交失败！{e}')
+    wait_time = random.randint(1, 300)  # Random wait time
+    print(f'为了避免同一时间签到人数太多导致被官方怀疑，开始休眠 {wait_time} 秒')
+    time.sleep(wait_time)   # Wait for random time to avoid ban
+    wallet = r.get(WalletURL, headers=headers)
+    print(
+        f"你当前拥有免费时长 {json.loads(wallet.text)['data']['free_time']['free_time']} 分钟，畅玩卡状态为 {json.loads(wallet.text)['data']['play_card']['short_msg']}，拥有米云币 {json.loads(wallet.text)['data']['coin']['coin_num']} 枚")
+    announcement = r.get(AnnouncementURL, headers=headers)
+    print(f'获取到公告列表：{json.loads(announcement.text)["data"]}')
+    res = r.get(NotificationURL, headers=headers)
+    try:
+        if list(json.loads(res.text)['data']['list']) == []:
+            success = True
+            Signed = True
+            Over = False
+        elif json.loads(json.loads(res.text)['data']['list'][0]['msg']) == {"num": 15, "over_num": 0, "type": 2, "msg": "每日登录奖励"}:
+            success = True
+            Signed = False
+            Over = False
+        elif json.loads(json.loads(res.text)['data']['list'][0]['msg'])['over_num'] > 0:
+            success = True
+            Signed = False
+            Over = True
+        else:
+            success = False
+    except IndexError:
+        success = False
+    if success:
+        if Signed:
+            print(
+                f'获取签到情况成功！今天是否已经签到过了呢？')
+            print(f'完整返回体为：{res.text}')
+        elif not Signed and Over:
+            print(
+                f'获取签到情况成功！当前免费时长已经达到上限！签到情况为{json.loads(res.text)["data"]["list"][0]["msg"]}')
+            print(f'完整返回体为：{res.text}')
+        else:
+            print(
+                f'获取签到情况成功！当前签到情况为{json.loads(res.text)["data"]["list"][0]["msg"]}')
+            print(f'完整返回体为：{res.text}')
+    else:
+        raise RunError(
+            f"签到失败！请带着本次运行的所有log内容到 https://github.com/ElainaMoe/MHYY-AutoCheckin/issues 发起issue解决（或者自行解决）。签到出错，返回信息如下：{res.text}")
