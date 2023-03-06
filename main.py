2b07a34528298031fd87c07c4f52ddbf5d9bcd9c
Pesy Wu <GamerNoTitle@outlook.com>
2023-03-06 13:01:04 +0800

    bug fix

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     print(f'为了避免同一时间签到人数太多导致被官方怀疑，开始休眠 {wait_time} 秒')
     time.sleep(wait_time)
     wallet = r.get(WalletURL, headers=headers)
-    if wallet.text == {"data":null,"message":"登录已失效，请重新登录","retcode":-100}: 
+    if wallet.text == {"data":,"message":"登录已失效，请重新登录","retcode":-100}: 
         print(f'当前登录已过期，请重新登陆！返回为：{wallet.text}')
     else:
         print(
